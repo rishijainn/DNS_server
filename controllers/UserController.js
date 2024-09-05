7a723e36d864be8f110b0884c7b8bc3190b710e1
Rishi Jain <jainrishirich2004@gmail.com>
2024-09-05 18:03:24 +0530

    commit for atlas database

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,5 +1,5 @@
 const Userdomain=require("../modles/UserModle");
-const bcrypt=require("bcryptjs");
+const bcrypt=require("bcrypt");
 const jwt=require("jsonwebtoken");
 require("dotenv").config();
 const secretKey=process.env.SecretKey;
